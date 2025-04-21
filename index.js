f5b9f75888b3a91ef845712a381ae80287776471
Ajay <kereajay578@gmail.com>
2025-04-21 15:14:23 +0530

    updated hosted url and cors

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.use(cors({
         "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://dailyneed-frontend.vercel.app"
       ],
       credentials:true,
 })
